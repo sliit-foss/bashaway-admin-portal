0a2d866db3fd35fd05049f6b916bd10138dd42e7
Akalanka <akalankaperera128@gmail.com>
2023-09-01 01:11:24 +0530

    Fix: empty constraint display

--- src/pages/question-details.jsx
+++ src/pages/question-details.jsx
@@ -52,7 +52,7 @@ export default function QuestionDetails() {
               <div className="flex flex-wrap gap-3 [&>span]:px-3 [&>span]:py-2 [&>span]:rounded-lg [&>span]:transition-all [&>span]:duration-medium">
                 <Footnote>{startCase(question?.difficulty?.toLowerCase())}</Footnote>
                 <Footnote>{question?.max_score}PT</Footnote>
-                <Footnote>{question?.constraints?.join(", ")}</Footnote>
+                {question?.constraints?.length && <Footnote>{question?.constraints?.join(", ")}</Footnote>}
               </div>
               <ActionButtons question={question} />
             </div>
